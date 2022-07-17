c20303f3e0efb9ef63c68281ff8190f80ee24fb3
Mohamed Alaa <mohamedelmenyawe606@gmail.com>
2022-07-17 11:10:52 +0200

    create api call , couins page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React from 'react';
-import './App.css';
+import React , { useState , useEffect } from 'react';
+import axios from 'axios';
+import Coins from './component/Coins';
 
 function App() {
+  const [coins,setCoins]=useState([]);
+
+  const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false'
+  
+  useEffect(() =>{
+    axios.get(url).then((response) => {
+      setCoins(response.data);
+      console.log(response.data[0])
+
+    }).catch(error =>{
+      console.log(error)
+    })
+  },[])
+
   return (
-    <div className="App">
-     <h1>Hallo</h1>
-    </div>
+    <>
+    <Coins coins={coins}/>
+    </>
   );
 }
 
---
+++ src/component/Coins.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import CoinItem from './CoinItem'
+
+const Coins = (props) => {
+  return (
+        <div className='container'>
+            <div>
+                <div className='heading'>
+                    <p>#</p>
+                    <p className='coin-name'>Coins</p>
+                    <p>Price</p>
+                    <p>24h</p>
+                    <p className='hide-mobile'>Volume</p>
+                    <p className='hide-mobile'>Mkt Cap</p>
+                </div>
+            </div>
+
+            {props.coins.map(coins =>{
+                return (
+                    <CoinItem coins={coins} key={coins.id}/>
+                )
+            })}
+           
+        </div>
+  )
+}
+
+export default Coins
